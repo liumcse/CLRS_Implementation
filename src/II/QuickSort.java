d9b916815e345d5d64c73bb5c6ff4578852f225d
Liu Mingyu <lyming90@gmail.com>
2017-10-03 17:37:16 +0800

    QuickSort

--- src/II/QuickSort.java
+++ src/II/QuickSort.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class QuickSort {
     public static void sort(int[] a, int p, int r) {
         if (p < r) {
-            int q = partition(a, p, r);
+            int q = partition_mid(a, p, r);
             sort(a, p, q - 1);
             sort(a, q + 1, r);
         }
@@ -14,9 +14,9 @@ public class QuickSort {
     public static int partition(int[] a, int p, int r) {
         // in this implementation, we choose a[r] as pivot
         int pivot = a[r];
-        int i = -1;  // i is (approx.) the index where the pivot should finally go to
+        int i = p - 1;  // i + 1 is the index where the pivot should finally go to
         // scan through the array
-        for (int j = 0; j < r; j++) {
+        for (int j = p; j < r; j++) {
             if (a[j] < pivot) {
                 i++;
                 swap(a, i, j);
@@ -25,6 +25,23 @@ public class QuickSort {
         // now i + 1 is the index where the pivot should go
         swap(a, i + 1, r);
         return i + 1;
+    }
+
+    public static int partition_mid(int[] a, int p, int r) {
+        // in this implementation, we choose the mid point as pivot
+        // This is in accordance with CZ2001
+        int mid = (p + r)/2;
+        swap(a, p, mid);
+        int pivot = a[p];
+        int i = p;
+        for (int j = p + 1; j <= r; j++) {
+            if (a[j] < pivot) {
+                i++;
+                swap(a, i, j);
+            }
+        }
+        swap(a, i, p);
+        return i;
     }
 
     private static void swap(int[] a, int x, int y) {
