2a6a0dcca2d7ed86b8f2561c9c011839bff7758e
Liu Mingyu <lyming90@gmail.com>
2017-10-13 12:28:01 +0800

    QuickSort

--- src/II/QuickSort.java
+++ src/II/QuickSort.java
@@ -1,5 +1,6 @@
 package II;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class QuickSort {
@@ -8,6 +9,14 @@ public class QuickSort {
             int q = partition_mid(a, p, r);
             sort(a, p, q - 1);
             sort(a, q + 1, r);
+        }
+    }
+
+    public static void sort_randomized(int[] a, int p, int r) {
+        if (p < r) {
+            int q = partition_randomized(a, p, r);
+            sort_randomized(a, p, q - 1);
+            sort_randomized(a, q + 1, r);
         }
     }
 
@@ -44,6 +53,25 @@ public class QuickSort {
         return i;
     }
 
+    public static int partition_randomized(int[] a, int p, int r) {
+        // in this implementation, we choose the pivot randomly
+        Random rand = new Random();
+        int pivotPos = rand.nextInt(r - p + 1) + p;
+        swap(a, pivotPos, r);
+        int pivot = a[r];
+        int i = p - 1;  // i + 1 is the index where the pivot should finally go to
+        // scan through the array
+        for (int j = p; j < r; j++) {
+            if (a[j] < pivot) {
+                i++;
+                swap(a, i, j);
+            }
+        }
+        // now i + 1 is the index where the pivot should go
+        swap(a, i + 1, r);
+        return i + 1;
+    }
+
     private static void swap(int[] a, int x, int y) {
         int temp = a[x];
         a[x] = a[y];
@@ -63,6 +91,14 @@ public class QuickSort {
         System.out.println("Please enter " + length + " numbers:");
         for (int i = 0; i < length; i++) a[i] = sc.nextInt();
 
+        System.out.println("Normal QuickSort:");
+        /* sort */
+        sort(a, 0, length - 1);
+
+        /* print array */
+        for (int i = 0; i < length; i++) System.out.print(a[i] + " ");
+
+        System.out.println("\nRandomized QuickSort");
         /* sort */
         sort(a, 0, length - 1);
 
